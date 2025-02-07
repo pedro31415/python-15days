21fbe52729bf3f9b3ccc49e1989a6188964d187a
Pedro <pedrogoku80@gmail.com>
2025-02-07 15:00:26 -0300

    condition

---
+++ study/condition.py
@@ -0,0 +1,15 @@
+num1 = 200
+
+num2 = 200
+
+
+if num1 > num2:
+    print("The number is ", num1)
+elif num1 == num2:
+    print("Same value")
+else:
+    print("The  number is ", num2)
+
+# short hand if
+
+if num1 >= num2: print("It's true guys")
