e2bcd1f0bb7ad34629f46ac426918fd030d32e43
Pedro <pedrogoku80@gmail.com>
2025-02-18 00:00:02 -0300

    tuple

---
+++ study/tuple.py
@@ -0,0 +1,14 @@
+thistuple = ("apple", "banana", "orange")
+
+print(thistuple)
+print(thistuple[2])
+
+# this code is wrong
+# thistuple[1] = "rogerio"
+# print(thistuple)
+
+thislist =list(thistuple)
+thislist.append("mango")
+
+print(thislist)
+
